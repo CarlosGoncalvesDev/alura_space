16915612f6f2cf6c9eaef31fce9c51c07b209189
carlos.goncalves <carloseduardo05.dev@gmail.com>
2025-01-02 22:33:22 -0300

    Retornando o código

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    # Adiciona 'range_values' no contexto para o template
+    return render(request, 'galeria/index.html', {'range_values': range(6)})
+
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
